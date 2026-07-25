8f01a20124032ee47e0b93158bdcefa4bb79f798
skechanhncoder-continuation <skechanhncoder@gmail.com>
2026-07-25 15:27:55 -0600

    added player fourth

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-def on_left_pressed():
-    pass
-controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
-
-mySprite = sprites.create(assets.image("""
-        Shitty Player
-        """),
-    SpriteKind.player)
-controller.move_sprite(mySprite)
+mp.set_player_sprite(mp.player_selector(mp.PlayerNumber.ONE),
+    sprites.create(assets.image("""
+            shitty player1
+            """),
+        SpriteKind.player))
+mp.set_player_sprite(mp.player_selector(mp.PlayerNumber.TWO),
+    sprites.create(assets.image("""
+            Shitty Player
+            """),
+        SpriteKind.player))
+mp.set_player_sprite(mp.player_selector(mp.PlayerNumber.THREE),
+    sprites.create(assets.image("""
+            shitty player2
+            """),
+        SpriteKind.player))
+mp.set_player_sprite(mp.player_selector(mp.PlayerNumber.FOUR),
+    sprites.create(assets.image("""
+            shitty playerr
+            """),
+        SpriteKind.player))
+mp.move_with_buttons(mp.player_selector(mp.PlayerNumber.ONE))
+mp.move_with_buttons(mp.player_selector(mp.PlayerNumber.TWO))
+mp.move_with_buttons(mp.player_selector(mp.PlayerNumber.THREE))
+mp.move_with_buttons(mp.player_selector(mp.PlayerNumber.FOUR))
--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`shitty player1`, SpriteKind.Player))
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Shitty Player`, SpriteKind.Player))
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`shitty player2`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`shitty playerr`, SpriteKind.Player))
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three))
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Four))
